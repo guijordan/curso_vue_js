fe951badf0d8db3b6ef27c87c36ef8351d1a9e0a
Guilherme Jordan De Oliveira <guijordanoliveira@hotmail.com>
2023-07-27 20:56:13 -0300

    Implementação botão ataque e ataque especial

--- curso_vue/projeto-01-monstro/app.js
+++ curso_vue/projeto-01-monstro/app.js
@@ -2,7 +2,7 @@ new Vue({
     el: '#app',
     data: {
         running: false,
-        playerLife: 0,
+        playerLife: 100,
         monsterLife: 100,
     },
     computed: {
@@ -11,9 +11,30 @@ new Vue({
         }
     },
     methods: {
+        startGame() {
+            this.running = true;
+            this.playerLife = 100;
+            this.monsterLife = 100;
+        },
 
+        attack(especial) {
+            this.hurt('monsterLife',5, 10, especial);
+            this.hurt('playerLife',7, 12, false);
+        },
+        hurt(prop, min, max, especial) {
+            const plus = especial ? 5 : 0;
+            const hurt = this.getRandom(min + plus, max + plus);
+            this[prop] = Math.max(this[prop] - hurt, 0);
+        },
+        getRandom(min, max) {
+            const value = Math.random() * (max - min) + min;
+            return Math.round(value);
+        }
     },
-    watch: {
 
+    watch: {
+        hasResult(value){
+            if(value) this.running = false;
+        }
     },
 });
