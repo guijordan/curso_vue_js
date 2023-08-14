23e37361eeb27e83ebe60e8d14c3b1e8852fbbc2
Guilherme Jordan De Oliveira <guijordanoliveira@hotmail.com>
2023-08-14 20:12:29 -0300

    Comunicação indireta com Callback

--- curso_vue/componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
+++ curso_vue/componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -4,6 +4,7 @@
         <p>Vários detalhes...</p>
         <p>Nome do usuário <strong>{{ inverterNome() }}</strong></p>
         <button @click="reinicializarNome">Reiniciar Nome</button>
+        <button @click="reiniciarFn()">Reiniciar Nome (Callback)</button>
     </div>
 </template>
 
@@ -17,6 +18,9 @@ export default {
             // default: function(){
             //     return Array(10).fill(0).join(',');
             // }
+        },
+        reiniciarFn: {
+            type:Function,
         }
     },
     methods: {
