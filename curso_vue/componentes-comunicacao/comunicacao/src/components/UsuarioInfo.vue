46afce7f3c9ac80e8a2e40feb9ac39bccefa05cb
Guilherme Jordan De Oliveira <guijordanoliveira@hotmail.com>
2023-08-14 20:05:54 -0300

    comunicação entre componentes utilizando eventos personalizados

--- curso_vue/componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
+++ curso_vue/componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -3,6 +3,7 @@
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
         <p>Nome do usuário <strong>{{ inverterNome() }}</strong></p>
+        <button @click="reinicializarNome">Reiniciar Nome</button>
     </div>
 </template>
 
@@ -18,18 +19,22 @@ export default {
             // }
         }
     },
-    methods:{
-        inverterNome(){
+    methods: {
+        inverterNome() {
             return this.nome.split('').reverse().join('');
+        },
+        reinicializarNome() {
+            this.nome = 'Pedro';
+            this.$emit('nomeMudou', this.nome);
         }
     }
 }
 </script>
 
 <style scoped>
-    .componente {
-        flex: 1;
-        background-color: #ec485f;
-        color: #fff;
-    }
+.componente {
+    flex: 1;
+    background-color: #ec485f;
+    color: #fff;
+}
 </style>
