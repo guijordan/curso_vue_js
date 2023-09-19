f8e982b142a76f06e2ccfe1183ddd3a9d1acb0cd
Guilherme Jordan De Oliveira <guijordanoliveira@hotmail.com>
2023-09-19 19:47:35 -0300

    Aula sobre rotas usando o framework vue-router para vue.js - Parte I

---
+++ curso_vue/rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="usuario-detalhe">
+        <h3>Usuário Detalhe</h3>
+        <p><strong>Código:</strong> {{ id }}</p>
+        <!-- :to="`/usuario/${id}/editar`" -->
+        <router-link tag="button" primario 
+        :to="{ name: 'editarUsuario', params: {id},
+               query: {completo : true, lingua: 'pt'} }">
+            Editar
+        </router-link>
+    </div>
+</template>
+<script>
+export default {
+    props:['id'],
+    // data(){
+    //     return {
+    //         id: this.$route.params.id
+    //     }
+    // },
+    // watch: {
+    //     $route(to, from){
+    //         this.id = to.params.id;
+    //     }
+    // },
+}
+</script>
+<style></style>
---
+++ curso_vue/rotas/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="usuario-editar">
+        <h3>Usuário Editar</h3>
+        <p><strong>Código:</strong> {{ id }} / {{ $route.params.id }}</p>
+        <p>Completo: {{ $route.query.completo ? 'Sim' : 'Não'}}</p>
+        <p>Língua: {{ $route.query.lingua }}</p>
+        <p></p>
+    </div>
+</template>
+<script>
+export default {
+    props:['id'],
+}
+</script>
+<style></style>
