53d8af8cf73fc276cea1a8e7248f57025a1d6756
Guilherme Jordan De Oliveira <guijordanoliveira@hotmail.com>
2023-11-01 20:11:48 -0300

    Problema de Monty Hall Parte II

--- curso_vue/vue/montyhall/src/App.vue
+++ curso_vue/vue/montyhall/src/App.vue
@@ -1,12 +1,38 @@
 <template>
     <div id="app">
         <h1>Problema de Monty Hall</h1>
+        <div class="form">
+            <div v-if="!started">
+                <label for="portsAmount">Quantas portas ?</label>
+                <input type="text" id="portsAmount" size="3" v-model.number="portsAmount" />
+            </div>
+            <div v-if="!stared">
+                <label for="selectedPort">Qual porta é premiada ?</label>
+                <input type="text" id="selectedPort" size="3" v-model.number="selectedPort" />
+            </div>
+            <button v-if="!started" @click="started = true">Iniciar</button>
+            <button v-if="started" @click="started = false">Reiniciar</button>
+        </div>
+        <div class="doors" v-if="started">
+            <div v-for="i in portsAmount" :key="i">
+                <Door :hasGift="i === selectedPort" :number="i" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import Door from './components/Door';
 export default {
-
+    name: 'App',
+    components: { Door },
+    data: function () {
+        return {
+            started: false,
+            portsAmount: 3,
+            selectedPort: null,
+        }
+    },
 }
 </script>
 
@@ -19,7 +45,7 @@ export default {
 
 body {
     color: #FFF;
-    background: linear-gradient(to right, rgb(21,153,87), rgb(21, 87, 153));
+    background: linear-gradient(to right, rgb(21, 153, 87), rgb(21, 87, 153));
 }
 
 #app {
@@ -34,5 +60,26 @@ body {
     background-color: #0004;
     padding: 20px;
     margin-bottom: 60px;
+}
+
+.form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 40px;
+}
+
+
+.form, .form input, .form button {
+    margin-bottom: 10px;
+    font-size: 2rem;
+}
+
+.doors{
+    align-self: stretch;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
 }
 </style>
