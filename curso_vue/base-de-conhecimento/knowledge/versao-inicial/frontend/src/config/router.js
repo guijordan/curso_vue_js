be74e7519acbb20af8ba27eb3a722c95126be3b9
Guilherme Jordan De Oliveira <guijordanoliveira@hotmail.com>
2023-12-18 20:25:40 -0300

    Base de Conhecimento - Frontend - Parte VII

--- curso_vue/base-de-conhecimento/knowledge/versao-inicial/frontend/src/config/router.js
+++ curso_vue/base-de-conhecimento/knowledge/versao-inicial/frontend/src/config/router.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router';
 
 import Home from '@/components/home/Home';
 import AdminPages from '@/components/admin/AdminPages';
+import ArticlesByCategory from '@/components/article/ArticlesByCategory';
+import ArticleById from '@/components/article/ArticleById';
 
 Vue.use(VueRouter);
 
@@ -10,10 +12,18 @@ const routes = [{
     name: 'home',
     path: '/',
     component: Home
-},{
-    name:'adminPages',
+}, {
+    name: 'adminPages',
     path: '/admin',
     component: AdminPages
+}, {
+    name: 'articlesByCategory',
+    path: '/categories/:id/articles',
+    component: ArticlesByCategory,
+}, {
+    name: 'articleById',
+    path: '/articles/:id',
+    component: ArticleById,
 }];
 
 export default new VueRouter({
--- curso_vue/base-de-conhecimento/knowledge/versao-inicial/frontend/src/main.js
+++ curso_vue/base-de-conhecimento/knowledge/versao-inicial/frontend/src/main.js
@@ -11,7 +11,7 @@ import router from './config/router';
 Vue.config.productionTip = false
 
 // TEMPORÁRIO !
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikxlb25hcmRvIE0uIExlaXTDo28iLCJlbWFpbCI6Imxlb25hcmRvQGNvZDNyLmNvbS5iciIsImFkbWluIjp0cnVlLCJpYXQiOjE3MDI1ODY2MTgsImV4cCI6MTcwMjg0NTgxOH0.c6vcvI3IXb4r1H8cQbq6CcUJD9P2BmsrSxN1fAZHq_0';
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikxlb25hcmRvIE0uIExlaXTDo28iLCJlbWFpbCI6Imxlb25hcmRvQGNvZDNyLmNvbS5iciIsImFkbWluIjp0cnVlLCJpYXQiOjE3MDI5MzY5NzgsImV4cCI6MTcwMzE5NjE3OH0.tysDUkQDc8zXLQDcw8ab5LEeCCI1Kn4FU5NYBL7awBo';
 
 new Vue({
   store,
